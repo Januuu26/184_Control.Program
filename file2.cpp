84a43e02de1e1b985d1a1032d1afd8e2d0c69939
Januuu26 <b77286116@gmail.com>
2026-03-09 18:54:42 +0700

    menambah string dan status lulus, tidak lulus

--- file2.cpp
+++ file2.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 using namespace std;
+
  float Rata_rata(float a, float b, float c){
     return (a+b+c)/2;
  }
+
+ string Status_lulus(float r){
+    if (r>=60)
+        return "Lulus";
+     else 
+        return "Tidak Lulus";
+    
+ }
+
+
+
+
+
+ 
